6addcfd0237b4e57f6f6974011f9e2d2221d369f
Versifiction <marc_charpentier@hotmail.fr>
2020-01-01 16:00:39 +0100

    edit port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 require("dotenv").config();
 
