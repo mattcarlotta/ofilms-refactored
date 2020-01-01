cdbbd2524987ae39089d1b3828fc6d8d6f323bdf
Versifiction <marc_charpentier@hotmail.fr>
2020-01-01 19:13:51 +0100

    env

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,10 @@ module.exports = function(app) {
   app.use(
     "/api",
     proxy({
-      target: "http://localhost:5000",
+      target:
+        process.env.NODE_ENV === "development"
+          ? "http://localhost:5000"
+          : "https://ofilms.herokuapp.com",
       changeOrigin: true
     })
   );
