66fc3100bef59d9013060707cf775c1f296d4783
Versifiction <marc_charpentier@hotmail.fr>
2020-01-01 14:31:21 +0100

    again

---
+++ build/precache-manifest.f55618ab8eef7261a4957a7131665a4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb02059830fd2f95391f694abbe8d7b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8a63ded067753f3563b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4374c5878522b26abb86",
+    "url": "/static/js/2.b9982a61.chunk.js"
+  },
+  {
+    "revision": "c8a63ded067753f3563b",
+    "url": "/static/js/main.b291a608.chunk.js"
+  },
+  {
+    "revision": "f126aa3a500fa60ef4a8",
+    "url": "/static/js/runtime-main.56660394.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
