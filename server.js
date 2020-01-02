404f4d10740810c738761e34b3ae3e30a740ae46
Matt <carlotta.matt@gmail.com>
2020-01-02 15:38:23 -0800

    initial commit

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+
 const port = process.env.PORT || 5000;
 
 require("dotenv").config();
 
 console.log("test ", process.env.TEST);
 console.log("env ", process.env.NODE_ENV);
-
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
 app.get("/api/message", (req, res) => {
   res.json({ message: "Test déploiement d'O'Films sur Heroku" });
@@ -21,6 +17,14 @@ app.get("/api/message", (req, res) => {
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello world !" });
 });
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is on up ${port} now !`);
